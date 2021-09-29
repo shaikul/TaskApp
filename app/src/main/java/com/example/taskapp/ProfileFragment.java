073b743a82c95c2f4f1fae0b5b9f71a95c199a04
shaikul <shaikul03@mail.ru>
2021-09-30 00:33:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ProfileFragment.java
@@ -0,0 +1,59 @@
+package com.example.taskapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.taskapp.databinding.FragmentProfileBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+    private ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater,container ,false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = view.findViewById(R.id.imageView);
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Selected image"),1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            Uri selected = data.getData();
+            if (!selected.equals(null)){
+                binding.imageView.setImageURI(selected);
+            }
+        }
+    }
+}
